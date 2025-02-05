3b611b3d2a28d471b72acfae7ebf893b78968e46
Travis Ennis <travis@travisennis.com>
2025-02-05 09:34:30 -0500

    feat: add type-safe objectFromEntries

--- source/object.ts
+++ source/object.ts
@@ -7,3 +7,9 @@ export function objectEntries<T extends object>(
 ): [keyof T, T[keyof T]][] {
   return Object.entries(obj) as [keyof T, T[keyof T]][];
 }
+
+export function objectFromEntries<K extends string | number | symbol, V>(
+  entries: [K, V][],
+): { [P in K]: V } {
+  return Object.fromEntries(entries) as { [P in K]: V };
+}
