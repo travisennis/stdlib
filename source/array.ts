fc70e8ae411a59b183966e245cc56ab2d5e9e7f5
Travis Ennis <travis@travisennis.com>
2025-03-22 15:20:22 -0400

    feat(array): add sortBy utility function

--- source/array.ts
+++ source/array.ts
@@ -1,3 +1,7 @@
 export function intersperse<A>(as: A[], separator: (index: number) => A): A[] {
   return as.flatMap((a, i) => (i ? [separator(i), a] : [a]));
 }
+
+export function sortBy<T>(array: T[], fn: (a: T) => number) {
+  return array.sort((a, b) => fn(a) - fn(b));
+}
